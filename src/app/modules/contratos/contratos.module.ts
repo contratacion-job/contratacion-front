5f694c8542455f94989e68357bfc7791e2a9a705
Danicv2000 <80560206+Danicv2000@users.noreply.github.com>
2025-05-04 10:59:38 -0400

    commit inicial

--- src/app/modules/contratos/contratos.module.ts
+++ src/app/modules/contratos/contratos.module.ts
@@ -6,6 +6,7 @@ import { ContratosRoutingModule } from './contratos-routing.module';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 
+
 @NgModule({
   declarations: [],
   imports: [
