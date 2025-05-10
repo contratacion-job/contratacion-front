17f6af903a5c0fa1db1b141a698d582b63c94a18
Danicv2000 <80560206+Danicv2000@users.noreply.github.com>
2025-05-10 16:33:46 -0400

    commit inicial

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContratoDashboardComponent } from './dashboard-contrato.component';
+import { DashboardProveedorComponent } from './dashboard-proveedor.component';
+import { DashboardSuplimentoComponent } from './dashboard-suplimento.component';
+import { DashboardEjecucionComponent } from './dashboard-ejecucion.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { NgApexchartsModule } from 'ng-apexcharts';
+
+
+
+@NgModule({
+  declarations: [
+    ContratoDashboardComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    NgApexchartsModule
+  ]
+})
+export class DashboardModule { }
